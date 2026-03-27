0a633afb070047a27225a82db0d46c9625846626
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-03-27 15:25:36 +0530

    logical operators

---
+++ basics/language_basics/logical_operators.py
@@ -0,0 +1,8 @@
+num1, num2, num3 = map(int, input().split())
+
+if num1 > num2 and num1 > num3:
+    print("Number 1 is greatest")
+elif num2 > num1 and num2 > num3:
+    print("Number 2 is greatest")
+else:
+    print("Number 3 is greatest")
