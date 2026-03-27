5e5a1b311079b07342c1fc833f062c8b4a939708
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-03-27 17:48:07 +0530

    Math operations

---
+++ basics/python_libraries/part2/mathematic_operations.py
@@ -0,0 +1,23 @@
+import math
+
+print(math.pi)
+print(math.e)
+
+# 2 ^ 3
+print(math.pow(2, 3))
+
+print(math.sqrt(16))
+
+print(math.factorial(5))
+
+print(math.gcd(10, 15))
+
+print(math.lcm(5, 10))
+
+print(math.ceil(10.7))
+
+print(math.floor(10.7))
+
+print(math.isfinite(float('inf')))
+
+print(math.isinf(float('inf')))
