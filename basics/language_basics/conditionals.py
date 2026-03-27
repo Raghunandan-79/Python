4de93faf3d99a86063d7ff34d0c492249cafe5f3
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-03-27 15:19:14 +0530

    Conditionals

---
+++ basics/language_basics/conditionals.py
@@ -0,0 +1,23 @@
+num1 = 20
+num2 = 50
+
+if num1 < num2:
+    print("num1 is smaller")
+
+print("End of if")
+
+if num1 < num2:
+    print("num1 is smaller")
+else:
+    print("num2 is smaller")
+
+print("End of if-else")
+
+if num1 < num2:
+    print("num1 is smaller")
+elif num1 > num2:
+    print("num2 is smaller")
+else:
+    print("Both are equal")
+
+print("End of if-elif")
