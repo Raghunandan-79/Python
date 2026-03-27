cdeee91d026ee3c81375c5bdb8df8dc855f1a7b3
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-03-27 15:11:24 +0530

    Variables

---
+++ basics/language_basics/variables.py
@@ -0,0 +1,62 @@
+num = 10 # integer
+print("Number is", num)
+
+# float
+num_float = 10.672
+print("Float num", num_float)
+
+# boolean
+is_true = True
+print("Boolean is", is_true)
+
+# string
+name = "Raj"
+name = 'raj'
+print(name + " is a boy")
+print(name + " is also known as Striver")
+print(name + " is currently running TUF")
+
+detail = "Raj's company is TUF"
+print(detail)
+
+description = 'the nickname of raj is "Striver"'
+print(description)
+
+first_name = "Raj"
+last_name = "Striver"
+full_name = first_name + ' ' + last_name
+print(full_name)
+
+name = "raj"
+result = 3 * name
+print(result)
+
+print(len(result))
+
+name = "striver"
+print(name[3])
+
+print(name[2:])
+
+print(name[:6])
+
+print(name[0::2])
+
+print(name + "\n")
+print("striver is raj \t raj is striver")
+
+description = "Raj\'s name is \"Striver\""
+print(description)
+
+description = "Raj is founder of \\ TUF"
+print(description)
+
+name = "ram"
+age = "19"
+college = "KIPS"
+print(f"His son's name is {name} and his age is {age} and he goes to college {college}")
+
+name = "striver"
+print(name.capitalize())
+print(name.upper())
+print(name.find("river"))
